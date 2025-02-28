9f1ee07cc8420ea7602c2bd0ae43e2b80b0e58a4
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2025-02-28 08:38:38 -0300

    use the root

--- oidc/create-role.sh
+++ oidc/create-role.sh
@@ -1,5 +1,5 @@
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-GITHUB_REPO_NAME=$(basename $(dirname $(pwd)))
+GITHUB_REPO_NAME=$(basename $(pwd))
 GITHUB_REPO="$GIT_HUB_USER_NAME/$GITHUB_REPO_NAME" # use in terminal => export GIT_HUB_USER_NAME="<Your-GIT-User"
 GITHUB_ACTION_ROLE_NAME=GitHubActionsRole-$GITHUB_REPO_NAME
 GITHUB_ACTION_POLICY_NAME=GitHubActionsPolicy-$GITHUB_REPO_NAME
